e7b40f4641e2a1cb05ce4e8f16a1a31c8269546a
Falaventho <falaventho@gmail.com>
2026-04-03 12:44:47 -0400

    version bump

--- src/cylutils/__init__.py
+++ src/cylutils/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Nicholas Gray"
 __copyright__ = "Copyright 2026, The Plasma Foundation Inc."
 __credits__ = ["Nick Gray"]
 __license__ = "Apache-2.0"
-__version__ = "v0.0.3"
+__version__ = "v0.0.4"
 __email__ = "Nicholas Gray <nicholas@tier2.tech>"
 __status__ = "Alpha"
 
