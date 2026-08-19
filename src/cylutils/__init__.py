2c155bdc987e40b24be608d1e6dbe7ea094e1c8e
Nicholas <64997940+Falaventho@users.noreply.github.com>
2026-08-19 13:14:42 -0400

    Dev (#7)
    bring repo up to maintainable standards

--- src/cylutils/__init__.py
+++ src/cylutils/__init__.py
@@ -1,9 +1,15 @@
+from importlib.metadata import PackageNotFoundError, version
+
 __author__ = "Nicholas Gray"
 __copyright__ = "Copyright 2026, The Plasma Foundation Inc."
-__credits__ = ["Nick Gray"]
+__credits__ = ["Nicholas Gray"]
 __license__ = "Apache-2.0"
-__version__ = "v0.1.0"
 __email__ = "Nicholas Gray <nicholas@tier2.tech>"
-__status__ = "Alpha"
+__status__ = "Development"
+
+try:
+    __version__ = version("cylutils")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
 
 from .simple_store import Store
